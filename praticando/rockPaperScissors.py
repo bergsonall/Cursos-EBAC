8e8d950843d305af3d6387b36918dce09715e624
bergsonall <bruninhodubal@hotmail.com>
2026-02-02 20:53:33 -0300

    line reduction

--- praticando/rockPaperScissors.py
+++ praticando/rockPaperScissors.py
@@ -20,13 +20,7 @@ while True:
     if user_opt == random_opt:
         draw += 1
         print(f'The computer played {random_opt} || Draw.')
-    elif user_opt == 'rock' and random_opt == 'scissors':
-        user_wins += 1
-        print(f'The computer played {random_opt} || You won.')
-    elif user_opt == 'paper' and random_opt == 'rock':
-        user_wins += 1
-        print(f'The computer played {random_opt} || You won.')
-    elif user_opt == 'scissors' and random_opt == 'paper':
+    elif user_opt == 'rock' and random_opt == 'scissors' or user_opt == 'paper' and random_opt == 'rock' or user_opt == 'scissors' and random_opt == 'paper':
         user_wins += 1
         print(f'The computer played {random_opt} || You won.')
     else:
