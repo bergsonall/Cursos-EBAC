7ecec1151b8a5d68e9c85945e7962e5b9e83238c
bergsonall <bruninhodubal@hotmail.com>
2026-02-08 19:04:43 -0300

    usando secret_key e criptografia para senhas

--- back-end/api/api_hashtag/main.py
+++ back-end/api/api_hashtag/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["argon2"], deprecated = "auto")
 
 # agora os imports funcionam
 from routes.auth_routes import auth_router
--- back-end/api/api_hashtag/routes/auth_routes.py
+++ back-end/api/api_hashtag/routes/auth_routes.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends
 import sys
 import os
+from main import bcrypt_context
+from models.models import Usuario 
+from routes.dependencies.dependencies import pegar_sessao
 
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 )
-
-from models.models import Usuario 
-from routes.dependencies.dependencies import pegar_sessao
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -21,7 +21,8 @@ async def criar_conta(nome : str, email : str, senha : str, session = Depends(pe
     if usuario:
         return {"message" : "Usuario já cadastrado"}
     else:
-        novo_usuario = Usuario(nome, email, senha)
+        senha_criptografada = bcrypt_context.hash(senha)
+        novo_usuario = Usuario(id=None, nome=nome, email=email, senha=senha_criptografada)
         session.add(novo_usuario)
         session.commit()
         return {"message" : "Sua conta foi criada com sucesso"}
